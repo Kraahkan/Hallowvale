e20add077ed2f0d7b6be10207b6c8af59c349e3e
J.D <kraahkan@gmail.com>
2015-11-28 13:20:32 -0800

    I've now ate my scarf and ascended to Godhood.  God bless

--- app/src/main/java/hallo/testproject/T_Rex.java
+++ app/src/main/java/hallo/testproject/T_Rex.java
@@ -2,17 +2,13 @@ package hallo.testproject;
 
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class T_Rex extends AppCompatActivity { // create a game
 
@@ -24,7 +20,7 @@ public class T_Rex extends AppCompatActivity { // create a game
     Spinner editRace;
 
     private String[] races;
-
+//GIT YOU SUCJ BRO
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
